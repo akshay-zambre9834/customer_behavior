95206868f1c2c74ba39e32bfbd5599fd4cc9b9fa
akshay zambare <akshayzambare88@yahoo.com>
2026-07-30 03:09:48 +0530

    Update customer_behavior.sql.sql

--- customer_behavior.sql.sql
+++ customer_behavior.sql.sql
@@ -1,9 +1,8 @@
-select * from customer;
 --1]what is a total revanue genarated by male vs female?
 select gender, sum (purchase_amount) as revanue
 from customer
 group by gender ;
---which cusstomer used a discount but still spend more than the avrage purchase amount?
+--2]which cusstomer used a discount but still spend more than the avrage purchase amount?
 SELECT customer_id, purchased_amount
 FROM customer
 WHERE discount_applied = 'Yes'
@@ -11,18 +10,18 @@ WHERE discount_applied = 'Yes'
       SELECT AVG(purchase_amount)
       FROM customer
   );
-  -- which are the top 5 products with the highest avrage rating ?
+  -- 3]which are the top 5 products with the highest avrage rating ?
   select item_purchased,round(avg (review_rating::numeric),2) as "avrage _product_rating"
   from customer 
   group by (item_purchased)
   order by avg (review_rating) desc
   limit 5;
-  -- compare the "avrage purches amount" between "standard" and "express shipping"
+  --4] compare the "avrage purches amount" between "standard" and "express shipping"
   select shipping_type,round(avg(purchase_amount::numeric),2) as "avrage_purchase_amount"
   from customer
   where shipping_type in ('Standard','express_shipping')
   group by shipping_type;
-  --do subsribed customer send more ? compare avrage spend and total revenue between subscriber and 
+  --5]do subsribed customer send more ? compare avrage spend and total revenue between subscriber and 
   --non subscribers 
 
 select subscription_status,
@@ -31,7 +30,7 @@ select subscription_status,
 		round(sum(purchase_amount),2) as "sum_of_puchase"
 		from customer
 		group by (subscription_status);
--- which 5 products are higher purches with discount applied 
+-- 6]which 5 products are higher purches with discount applied 
 select 
         item_purchased,
         COUNT(*) as total_purchases
@@ -49,10 +48,11 @@ GROUP BY item_purchased
 ORDER BY total_purchases DESC
 LIMIT 5;
 --.........................
--- segment customers into new returning and loyal based on their total number of privious purches
+
 --and show the count of each segment?
---जेव्हा प्रश्नात असे शब्द असतात:Segment,Categorize,Classify,Bucket
+--in quetion that word in used :Segment,Categorize,Classify,Bucket
 ---->                              	think on case,when suddenly
+-- 7]segment customers into new returning and loyal based on their total number of privious purches
 select 
 case 
   when previous_purchases=0 then 'new'
@@ -76,33 +76,20 @@ from customer
 select customer_segment ,count (*) as number_of_customers 
 from customer_type
 group by customer_segment;
-
-
-
-
-
-
-
-
-
-
-
-
-  
 --........................
---जेव्हा प्रश्नात असे शब्द असतात:
+--in quetion we found that type of words :
 --How many
 --Count
 --Total customers
---        तेव्हा COUNT(*).
+--        then COUNT(*).
 --........................
 --Each segment
 --Each category
 --Each department
---        तेव्हा GROUP BY.
+--        then GROUP BY.
 --........................
 
---what are the top 3 most purchesed products within each category 
+--8]what are the top 3 most purchesed products within each category 
 WITH product_sales AS (
     SELECT
         category,
@@ -135,20 +122,35 @@ ORDER BY category, rn;
 --Top N within each group → ROW_NUMBER() / RANK() + PARTITION BY
 
 
--- ARE customers who are repeat buyers(more than 5 privious purches)also likely to subscribe 
+-- 9]ARE customers who are repeat buyers(more than 5 privious purches)also likely to subscribe 
 select subscription_status,
 count(customer_id) AS "repeat_buyers"
 from customer 
 where previous_purchases >5
 group by subscription_status
 
--- what is revenue contribution of each age group ?
+--10] what is revenue contribution of each age group ?
 SELECT
     "age group",
     SUM(purchase_amount) AS total_revenue
 FROM customer
 GROUP BY "age group"
 ORDER BY total_revenue DESC;
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
 
 
 
